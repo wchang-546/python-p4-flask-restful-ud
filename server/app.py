582e500a0945b25b8dae3b802e9cf7b712268ffc
Will Chang <william.chaang@gmail.com>
2023-08-16 13:54:44 -0600

    Code along done

--- server/app.py
+++ server/app.py
@@ -80,6 +80,32 @@ class NewsletterByID(Resource):
 
         return response
 
+    def patch(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form.get(attr))
+        
+        db.session.add(record)
+        db.session.commit()
+
+        response = make_response(
+            record.to_dict(),
+            201
+        )
+        return response
+    
+    def delete(self, id): 
+        record = Newsletter.query.filter_by(id=id).first()
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "Deleted"},
+        response = make_response(
+            response_dict,
+            200
+        )
+        return response
+
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 
